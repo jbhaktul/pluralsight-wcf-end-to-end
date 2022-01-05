bc3ec83a5a88d93966334de1bfdb09fe9180caf2
john.e.bhaktul.ctr <john.e.bhaktul.ctr@nga.mil>
2022-01-05 08:57:38 -0500

    Writing the unit tests - End of Module 3.

---
+++ WCF-End-to-End/GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Data;
+using GeoLib.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "Herndon",
+                State = new State() { Abbreviation = "VA" },
+                Zip = "20166"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("20166")).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+            ZipCodeData data = geoService.GetZipInfo("20166");
+
+            Assert.IsTrue(data.City.ToUpper() == "HERNDON");
+            Assert.IsTrue(data.State == "VA");
+        }
+    }
+}
